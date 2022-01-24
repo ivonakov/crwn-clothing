e87145a2c9628d31bf18bcd85b1697af5774ee18
ivonakov <ivonakov@gmail.com>
2022-01-24 23:42:55 +0200

    91. Custom Button Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
+
 
 
 class SignIn extends React.Component {
@@ -51,7 +54,7 @@ class SignIn extends React.Component {
                         required
                     />
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'> Sign In </CustomButton>
                 </form>
             </div>
         );
