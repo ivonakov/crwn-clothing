fe8186b34a99e4ea7a22a79eb3dd258a639adaa1
ivonakov <ivonakov@gmail.com>
2022-01-24 22:22:16 +0200

    86. Header Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-;
 
- 
+
+
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
