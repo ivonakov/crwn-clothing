e3cb8d7781a48105da9031926031fbbaf6b25f52
ivonakov <ivonakov@gmail.com>
2022-01-29 23:18:52 +0200

    126. Implementing Redux In Cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss'
 
 
-const Header = ( { currentUser } ) => (
+const Header = ( { currentUser, hidden } ) => (
 
 
     <div className='header'>
@@ -37,12 +37,12 @@ const Header = ( { currentUser } ) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        { hidden ? null : <CartDropdown /> }
     </div>
 )
 
-const mapStateToProps = state => ( {
-    currentUser: state.user.currentUser
+const mapStateToProps = ( { user: { currentUser }, cart: { hidden } } ) => ( {
+    currentUser, hidden
 } )
 
 export default connect( mapStateToProps )( Header )
